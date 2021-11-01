5f1c1685b435283d56ba3222121c819e9b2572e7
Clinery1 <clinery8237@gmail.com>
2021-11-01 14:49:58 -0400

    Fixed an incompatibility Firefox does not have a `getInnerHTML` function. This was just used to look pretty. Changed to `innerHTML` property

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ var globalList;
 
 
 function showItemList(elem,list) {
-    console.log(list);
-    console.log(elem);
     if (elem.listVisibility) {
         let listElem=elem.firstElementChild;
-        let text=listElem.getInnerHTML();
+        let text=listElem.innerHTML;
         listElem.innerHTML="▸"+text.substring(1);
         elem.listVisibility=false;
         list.style.fontSize="";
     } else {
         let listElem=elem.firstElementChild;
-        let text=listElem.getInnerHTML();
+        let text=listElem.innerHTML;
         listElem.innerHTML="▾"+text.substring(1);
         elem.listVisibility=true;
         list.style.fontSize="medium";
